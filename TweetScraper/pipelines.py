cbae556b5368814c687df7979c73010dcaf2e8bb
Ben <bdjohnson529@gmail.com>
2018-06-20 15:14:34 -0700

    added savetomysql pipeline

--- TweetScraper/pipelines.py
+++ TweetScraper/pipelines.py
@@ -6,6 +6,10 @@ import pymongo
 import json
 import os
 
+# for mysql
+import mysql.connector
+from mysql.connector import errorcode
+
 from TweetScraper.items import Tweet, User
 from TweetScraper.utils import mkdirs
 
@@ -52,6 +56,117 @@ class SaveToMongoPipeline(object):
         else:
             logger.info("Item type is not recognized! type = %s" %type(item))
 
+
+class SavetoMySQLPipeline(object):
+
+    ''' pipeline that save data to mysql '''
+    def __init__(self):
+        # connect to mysql server
+        user = raw_input("MySQL User: ")
+        pwd = raw_input("Password: ")
+        self.cnx = mysql.connector.connect(user=user, password=pwd,
+                                host='localhost',
+                                database='tweets', buffered=True)
+        self.cursor = self.cnx.cursor()
+        self.table_name = raw_input("Table name: ")
+        create_table_query =   "CREATE TABLE `" + self.table_name + "` (\
+                `ID` CHAR(20) NOT NULL,\
+                `url` VARCHAR(140) NOT NULL,\
+                `datetime` VARCHAR(22),\
+                `text` VARCHAR(280),\
+                `user_id` CHAR(20) NOT NULL,\
+                `usernameTweet` VARCHAR(20) NOT NULL\
+                )"
+
+        try:
+            print "Creating table..."
+            self.cursor.execute(create_table_query)
+        except mysql.connector.Error as err:
+            print err.msg
+        else:
+            self.cnx.commit()
+            print "Successfully created table."
+
+    def find_one(self, trait, value):
+        select_query =  "SELECT " + trait + " FROM " + self.table_name + " WHERE " + trait + " = " + value + ";"
+        try:
+            val = self.cursor.execute(select_query)
+        except mysql.connector.Error as err:
+            return False
+        
+        if (val == None):
+            return False
+        else:
+            return True
+
+    def check_vals(self, item):
+        ID = item['ID']
+        url = item['url']
+        datetime = item['datetime']
+        text = item['text']
+        user_id = item['user_id']
+        username = item['usernameTweet']
+
+        if (ID is None):
+            return False
+        elif (user_id is None):
+            return False
+        elif (url is None):
+            return False
+        elif (text is None):
+            return False
+        elif (username is None):
+            return False
+        elif (datetime is None):
+            return False
+        else:
+            return True
+
+
+    def insert_one(self, item):
+        ret = self.check_vals(item)
+
+        if not ret:
+            return None
+
+        ID = item['ID']
+        user_id = item['user_id']
+        url = item['url']
+        text = item['text']
+        username = item['usernameTweet']
+        datetime = item['datetime']
+
+        insert_query =  "INSERT INTO " + self.table_name + " (ID, url, datetime, text, user_id, usernameTweet )"
+        insert_query += " VALUES ( '" + ID + "', '" + url + "', '"
+        insert_query += datetime + "', '" + text + "', '" + user_id + "', '" + username + "' )"
+
+        print insert_query
+
+        try:
+            print "Inserting..."
+            self.cursor.execute(insert_query)
+        except mysql.connector.Error as err:
+            print err.msg
+        else:
+            print "Successfully inserted."
+            self.cnx.commit()
+
+
+    def process_item(self, item, spider):
+        print "now only made it to process item"
+        if isinstance(item, Tweet):
+            print item['ID']
+            dbItem = self.find_one('user_id', item['ID'])
+            if dbItem:
+                print "skipping"
+                pass # simply skip existing items
+                ### or you can update the tweet, if you don't want to skip:
+                # dbItem.update(dict(item))
+                # self.tweetCollection.save(dbItem)
+                # logger.info("Update tweet:%s"%dbItem['url'])
+            else:
+                self.insert_one(dict(item))
+                logger.debug("Add tweet:%s" %item['url'])
 
 
 class SaveToFilePipeline(object):
